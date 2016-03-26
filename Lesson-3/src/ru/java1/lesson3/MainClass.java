93dd83925aad34da2daff89d289b541bec6eddfe
Abilis <abilis@mail.ru>
2016-03-26 14:46:15 +0400

    Первый коммит. Проверка побитовых И и ИЛИ

---
+++ Lesson-3/src/ru/java1/lesson3/MainClass.java
@@ -0,0 +1,18 @@
+package ru.java1.lesson3;
+
+/**
+ * Created by Abilis on 26.03.2016.
+ */
+public class MainClass {
+
+    public static void main(String[] args) {
+
+        int a = 12;
+        int b = 10;
+
+        int c = a | b;
+        System.out.println(c);
+
+    }
+
+}
