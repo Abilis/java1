b699baf49b4c5f4ad2e3e3d9ae534bd3daadbb52
Abilis <abilis@mail.ru>
2016-03-28 11:57:21 +0400

    Написана упаковка IP-адреса в одно число типа int

--- Lesson-3/src/ru/java1/lesson3/MainClass.java
+++ Lesson-3/src/ru/java1/lesson3/MainClass.java
@@ -7,11 +7,38 @@ public class MainClass {
 
     public static void main(String[] args) {
 
-        int a = 12;
-        int b = 10;
+        //192.168.100.10
+        int ipAddress = 192; //записываем в интеджер число 192 (11 000 000)
+        ipAddress = ipAddress << 8; //сдвигаем биты на 8 разрядов влево
 
-        int c = a | b;
-        System.out.println(c);
+        ipAddress += 168; //добавляем в исходный интеджер 168 ( 10 101 000)
+        ipAddress = ipAddress << 8; //сдвигаем биты еще на 8 разрядов влево
+
+        ipAddress += 10; //добавляем в исходный интеджер 100 (01 100 100)
+        ipAddress = ipAddress << 8; //сдвигаем биты еще на 8 разрядов влево
+        ipAddress += 111; //добавляем в исходный интеджер еще 10 (00 001 010)
+
+        System.out.println(ipAddress + " - так записан 192.168.100.10, \"упакованный\" в один интеджер"); //что теперь записано в ipAddress
+        String ipAddressAsStr = Integer.toBinaryString(ipAddress); //в двоичном виде
+
+        String firstOctet = ipAddressAsStr.substring(0, 8);
+        String secondOctet = ipAddressAsStr.substring(8, 16);
+        String thirdOctet = ipAddressAsStr.substring(16, 24);
+        String fourthOctet = ipAddressAsStr.substring(24, 32);
+
+        //строковое представление в двоичном виде
+        System.out.println(firstOctet + "." + secondOctet + "." + thirdOctet + "." + fourthOctet + " - так он выглядит внутри, только без точек");
+
+        System.out.println();
+
+        //Восстанавливаем IP-адрес из интеджера
+        int fourthOctetInt = ipAddress & 255; //должен получиться последний октет
+        int thirdOctetInt = (ipAddress >> 8) & 255; //сдвигаем исходный интеджер на 8 бит вправо и вытаскиваем 3 октет
+        int secondOctetInt = (ipAddress >> 16) & 255; //сдвигаем исходный интеджер на 16 бит вправо и вытаскиваем 2 октет
+        int firstOctetInt = (ipAddress >> 24) & 255; //сдвигаем исходный интеджер на 24 бита вправо и вытаскиваем 1 октет
+
+        //смотрим что получилось
+        System.out.println(firstOctetInt + "." + secondOctetInt + "." + thirdOctetInt + "." + fourthOctetInt + " - востановленный адрес");
 
     }
 
