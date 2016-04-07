5679aeac16a4ef94f8688ee62c01d04b5f47d8fe
Abilis <abilis@mail.ru>
2016-04-07 21:36:08 +0400

    Еще раз написан метод бинарного поиска

--- Lesson4-1/src/ru/java1/lesson4_1/Main.java
+++ Lesson4-1/src/ru/java1/lesson4_1/Main.java
@@ -21,8 +21,8 @@ public class Main {
         System.out.println(getIndex(null, 100));
 
         System.out.println();
-        System.out.println(getIndexBinary(arr, 2));
-        System.out.println(getIndexBinary(arr, 51));
+        System.out.println(getIndexBinary(arr, 6));
+        System.out.println(getIndexBinary(arr, 50));
         System.out.println(getIndexBinary(null, 100));
 
         System.out.println();
@@ -33,9 +33,9 @@ public class Main {
         System.out.println();
         System.out.println();
 
-        System.out.println(getPosishn(arr, 2));
-        System.out.println(getPosishn(arr, 51));
-        System.out.println(getPosishn(null, 100));
+//        System.out.println(getPosishn(arr, 2));
+//        System.out.println(getPosishn(arr, 51));
+//        System.out.println(getPosishn(null, 100));
 
     }
 
@@ -63,55 +63,26 @@ public class Main {
         int minIndex = 0;
         int maxIndex = arr.length - 1;
 
-        while (minIndex <= maxIndex) { //последняя итерация цикла - когда индексы стали равны друг другу
+        while (minIndex <= maxIndex) {
 
-            int midIndex = minIndex + (maxIndex - minIndex) / 2; //находим индекс, который будет обозначать середину
+            int midIndex = minIndex + (maxIndex - minIndex) / 2;
 
-            if (value < arr[midIndex]) { //если значение меньше значения в середине
-                maxIndex = midIndex - 1;   //то теперь будем искать слева от середины
+            if (value < arr[midIndex]) {
+                maxIndex = midIndex - 1;
             }
-            else if (value > arr[midIndex]) {   //если же значение больше значения в середине
-                    minIndex = midIndex + 1;   //то теперь будем искать справа от середины
+            else if (value > arr[midIndex]) {
+                minIndex = midIndex + 1;
             }
             else {
-                return midIndex; //если же значение равно тому, что в середине, значит, мы нашли то, что искали
-                                    //по условию достаточно вернуть любой индекс, если значений в массиве несколько
+                return midIndex;
             }
+
         }
 
-        return -1; //ничего не нашли
+        return -1;
 
     }
 
-    public static int getPosishn(int[] arr, int a) {
-        int possision = -1;
 
-        try {
-
-
-            int l;
-            int end = arr.length;
-            int i = end / 2 ;
-
-            while (i >= 0 && i < end) {
-
-                if (i == 0 && arr[i] != a) return  possision;
-                if (arr[i] == a) return i;
-                else if (arr[i] > a) {
-                    l = end - i;
-                    i = (l / 2) + l;
-                } else if (arr[i] < a) {
-                    l = i;
-                    i = l + (l / 2);
-                }
-
-
-            }
-        }catch (Exception e){
-            System.out.println(e.getMessage());
-            throw  e;
-        }
-        return possision;
-    }
 
 }
